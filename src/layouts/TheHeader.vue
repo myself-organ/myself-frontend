b03005c2f98d241226e511e19e5dbb63ea14b790
Aleksander Afanasyev <afonia.marcii@gmail.com>
2025-02-06 19:34:48 +0100

    feat: update TheHeader component; modify logo link text, enhance menu structure, and improve styling for better usability

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -1,36 +1,38 @@
 <template>
   <header class="header">
-    <a class="logo-btn" href="/">
-      <span>Afanasev Aleksander</span>
+    <a class="menu__link" href="/">
+      <span>Alexander Afanasjev</span>
     </a>
     <nav class="menu">
-      <!-- <ul class="menu-list">
-        <li class="menu__item"><a href="/" class="menu__link menu__link--active">Home</a></li>
-        <li class="menu__item"><a href="/" class="menu__link">CV</a></li>
-        <li class="menu__item">
-          <a href="/" class="menu__link">Contact</a>
-        </li>
-      </ul> -->
       <ul class="menu-list">
         <li class="menu__item">
-          <button @click="toggleTheme" class="theme-toggle">
-            {{ currentTheme === 'light' ? '🌙' : '☀️' }}
-          </button>
+          <a
+            href="https://github.com/SubwaySerfer"
+            class="menu__link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub</a
+          >
+        </li>
+        <li class="menu__item">
+          <router-link to="/" class="menu__link" style="cursor: no-drop"
+            >Contact</router-link
+          >
         </li>
       </ul>
     </nav>
   </header>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style scoped>
 .header {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1rem;
+  padding: 1.75rem 2rem;
 }
 
 .menu-list {
@@ -42,46 +44,10 @@
 }
 
 .menu__link {
+  font-weight: 500;
+  font-size: 1.19rem;
+  line-height: 120%;
   color: var(--color-white-light);
-  border-radius: 2px;
-  padding: 0.15em 0.5em;
-  text-decoration: underline;
-  transition: opacity 0.3s;
-  font-size: 1.25rem;
-  line-height: 1.5;
-  opacity: 0.6;
-  border-radius: 2px;
-  &:hover {
-    opacity: 1;
-  }
-}
-.menu__link--active {
-  opacity: 1;
-  /* background: var(--color-primary); */
-}
-
-.logo-btn {
-  background: transparent;
   cursor: pointer;
-  transition: opacity 0.3s;
-  color: var(--color-white-light);
-  font-size: 1.5rem;
-  line-height: 1.5;
-  &:hover {
-    opacity: 0.6;
-  }
-}
-.theme-toggle {
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  font-size: 1.25rem;
-  padding: 0.15em 0.5em;
-  border-radius: 2px;
-  transition: opacity 0.3s;
-  opacity: 0.6;
-  &:hover {
-    opacity: 1;
-  }
 }
 </style>
